c5c51651f9091ba201d1a932a747c90cf14860cb
Karishma Reddy Khan <kreddykhan@brandeis.edu>
2016-11-23 20:11:00 -0500

    test interface implemented

--- app.rb
+++ app.rb
@@ -4,17 +4,19 @@ require './config/environments'
 require './models/user'
 require './models/tweet'
 require './models/relationship'
+require 'csv'
+require 'faker'
 
 enable :sessions
 set :session_secret, "super secret"
 
 get '/' do
     @users = User.all
-    @users.each do |user|
-        # puts user.id
-        # puts user.username
-        # puts user.firstname
-    end
+    # @users.each do |user|
+    #     puts user.id
+    #     puts user.username
+    #     puts user.firstname
+    # end
   erb :index
 end
 
@@ -78,6 +80,86 @@ post '/tweet' do
     end
 end
 
+############################## Test Environment ##############################
+
+get '/test/reset/all' do
+    User.delete_all
+    Tweet.delete_all
+    Relationship.delete_all
+    @user = User.new(:username => "testuser", :password_digest => "password", :firstname => "test", :lastname => "user", :email => "testuser@sample.com", :birthday => "01/01/1990")
+    @user.save
+    redirect '/'
+end
+
+get '/test/reset/testuser' do
+    @user = User.find_by(:username => 'testuser')
+    if @user!=nil
+        Tweet.where(user_id: @user.id).delete_all
+        Relationship.where(user_id: @user.id).delete_all
+        @user.delete
+    end
+    @user = User.new(:username => "testuser", :password_digest => "password", :firstname => "test", :lastname => "user", :email => "testuser@sample.com", :birthday => "01/01/1990")
+    @user.save
+    redirect '/'
+end
+
+get '/test/status' do
+    erb :status
+end
+
+get '/test/reset/standard' do
+    User.delete_all
+    Tweet.delete_all
+    Relationship.delete_all
+    load 'db/seeds.rb'
+    n = params[:tweets]
+    if n!=nil
+        tweet_number(n)
+    end
+    redirect '/test/status'
+end
+
+get '/test/users/create' do
+    user_number = params[:count].to_i
+    tweet_number = params[:tweets].to_i
+
+    if user_number == nil
+        user_number = 1
+    end
+
+    if tweet_number == nil
+        tweet_number = 0
+    end
+
+    user_number.times do
+        first_name = Faker::Name.first_name
+        params = {:username => first_name, :password => 'test', :firstname => first_name, :lastname => Faker::Name.last_name, :email => Faker::Internet.email(first_name), :birthday => Faker::Date.backward(20)}
+        @user = User.new(params)
+        @user.save
+        tweet_number.times do
+            params = {:user_id => @user.id, :body => Faker::Lorem.sentence}
+            @tweet = Tweet.new(params)
+            @tweet.save
+        end
+    end
+    redirect '/'
+end
+
+get '/test/user/follow' do
+    @users = User.all
+    follow_number = params[:count].to_i
+    follow_number.times do
+        @user1 = @users.sample
+        follow_number.times do
+            @user2 = @users.sample
+            @user1.followed << @user2
+        end
+    end
+    redirect '/'
+end
+
+############################## Helper Methods ##############################
+
 helpers do
   def logged_in?
     !!session[:user_id]
@@ -85,5 +167,17 @@ helpers do
 
   def current_user
     User.find(session[:user_id])
+  end
+
+  def tweet_number(n)
+      n = n.to_i
+      Tweet.delete_all
+      csv_text = File.read('seeds/tweets.csv')
+      csv_tweets = CSV.parse(csv_text, :encoding => 'ISO-8859-1')
+      for i in 1...n
+          row = csv_tweets[i]
+          @tweet = Tweet.new(:user_id => row[0], :body => row[1], :date => row[2])
+          @tweet.save
+      end
   end
 end
--- models/user.rb
+++ models/user.rb
@@ -6,10 +6,14 @@ class User < ActiveRecord::Base
 
   has_many :tweets
 
+  # has many relationships through a foreign key which is a follower_id number
   has_many :relationships, foreign_key: :follower_id
+  # has many people they are following through the relationships table entries
   has_many :followed, through: :relationships, source: :followed
 
+  # has many relationships through a foreign key which is the followed_id number of the Relationship class
   has_many :reverse_relationships, foreign_key: :followed_id, class_name: 'Relationship'
+  # has many followers through the relationships
   has_many :followers, through: :reverse_relationships, source: :follower
 
   def follow!(user)
