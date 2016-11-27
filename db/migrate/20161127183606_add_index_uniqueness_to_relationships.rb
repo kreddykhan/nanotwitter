52dccfb41808ee8ba1c1b52d6a4d62e2be930324
Karishma Reddy Khan <kreddykhan@brandeis.edu>
2016-11-27 15:56:06 -0500

    heroku attempt

--- db/migrate/20161127183606_add_index_uniqueness_to_relationships.rb
+++ db/migrate/20161127183606_add_index_uniqueness_to_relationships.rb
@@ -1,5 +1,12 @@
 class AddIndexUniquenessToRelationships < ActiveRecord::Migration
   def change
+      Tweet.destroy_all
+      Relationship.destroy_all
+      User.destroy_all
+
+      ActiveRecord::Base.connection.tables.each do |t|
+          ActiveRecord::Base.connection.reset_pk_sequence!(t)
+      end
       add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
